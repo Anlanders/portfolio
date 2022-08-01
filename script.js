39bc64b1f9967f51678a8f65dc8b6f1d8a1128d7
Anlander <101355241+Anlanders@users.noreply.github.com>
2022-08-01 06:01:23 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-
-themeButton.onclick = function() {
-  page.classList.toggle('pro-light-theme');
-  page.classList.toggle('pro-dark-theme');
-};
-pro
